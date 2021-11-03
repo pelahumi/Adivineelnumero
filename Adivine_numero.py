161966eb6764ad13ebce8846bed7d114be788523
Pelayo <phuermij@myuax.com>
2021-11-03 14:20:03 +0100

    prueba1

--- Adivine_numero.py
+++ Adivine_numero.py
@@ -1,7 +1,28 @@
 import random
 numero = random.randint(0,99)
 intento = int(input("Introduce un número: "))
+total_intentos = 1
 
-def estar_en_rango (rango, intento):
-    return intento in range(0,99)
-print(estar_en_rango([0,100],intento))
+while intento < 0 or intento > 99:
+    print("Error el número tiene que estar entre 0 y 99")  
+    intento = int(input("Introduce un número: "))
+
+if intento == numero:
+    print("Enhorabuena has acertado")
+
+while intento > numero:
+    print("Demasiado grande")
+    total_intentos = total_intentos + 1
+    intento = int(input("Introduce un número: "))
+    if intento == numero:
+        print("Enhorabuena has acertado")
+
+while intento < numero:
+    print("Demasiado pequeño")
+    total_intentos = total_intentos + 1
+    intento = int(input("Introduce un número: "))
+    if intento == numero:
+        print("Enhorabuena has acertado")
+        total_intentos = total_intentos + 1
+
+print(total_intentos)
