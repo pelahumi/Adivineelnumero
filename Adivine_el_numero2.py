9b8d9d1686d2734d54fe44b528d2e1709ed0fe1a
Pelayo <phuermij@myuax.com>
2021-11-12 17:09:23 +0100

    Primera funcion

--- Adivine_el_numero2.py
+++ Adivine_el_numero2.py
@@ -1,34 +1,25 @@
 import random
 numero = random.randint(0,100)
 
+def pedir_numero():
+    while True:
+        entrada = input("Introduzca un número entre el 0 y 99: ")
+        try:
+            entrada = int(entrada)
+        except:
+            pass
+        else:
+            if 0 <= entrada <= 99:
+                break
+    return entrada
+
 print("Introduzca un número: ")
-
-while True:
-    numero = input("Introduzca un número entre el 0 y 99 incluídos")
-
-    try:
-        numero = int(numero)
-    except:
-        pass
-    else: 
-        if 0 <= numero <= 99:
-            break
+numero = pedir_numero()
 
 print("Intente encontrar el numero a adivinar: ")
 
 while True:
-    while True:
-        intento = input("Introduzca un número entre el 0 y 99 incluídos: ")
-
-        try:
-            numero = int(numero)
-        except:
-            pass
-        else:
-            if 0 <= numero <=99:
-                break
-
-                
+    intento = pedir_numero()
     if intento < numero:
         print("Demasiado pequeño")
     elif intento > numero:
